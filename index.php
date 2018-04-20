d00019992d47eaeb3777c78dd736f55f85ba7770
JDonald <sanderrusel@gmail.com>
2018-04-20 13:37:51 -0600

    mi primer commt

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Sitio turistico, termales de tipitapa"/>
+        <meta name="keywords" content="Termales, Centro turistico, Tipitapa"/>
+        <meta name="author" content="Donald Munguía, Lexmark Palacios, Amy Rodriguez"/>
+        <meta name="viewport" width=device-width, initial-scale=1.0/>        
+        <title>Página principal</title>
+    </head>
+    <body>
+        <footer>
+
+        </footer>
+    </body>
+</html>
+
+?>
